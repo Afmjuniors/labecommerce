a1a2ead2d8746b569c06678da9e9447f0741fe3f
Afmjuniors <afmjuniors@gmail.com>
2023-01-23 15:44:08 -0300

    feat: refatorando codigo TS para incorporar knex

--- src/types.ts
+++ src/types.ts
@@ -1,24 +1,43 @@
 export type TUser = {
     id: string,
+    name:string,
     email: string,
-    password: string
+    password: string,
+    modifiedAt:string,
+    createAt: string,
 }
 
 export type TProduct = {
     id: string,
     name: string,
     price: number,
+    description: string,
+    urlImage:string,
+    modifiedAt:string,
+    createAt: string,
     category: Category
 }
 
 export type TPurchase = {
-    userId: string,
-    productId: string,
-    quantity: number,
-    totalPrice: Number
+    id:string,
+    buyerID: string,
+    totalPrice: Number,
+    paid:number,
+    createAt:string,
+    modifiedAt:string,
+    deliveredAt:string
 }
+
+export type TPurchaseProduct ={
+    purchaseId:string,
+    productID:string,
+    subTotal:number,
+    quantity:number
+}
+
 export enum Category{
     ACCESSORIES = "Acessórios",
     CLOTHES_AND_SHOES = "Roupas e calçados",
     ELECTRONICS = "Eletrônicos"
 }
+await db.raw(`SELECT * FROM users WHERE id="${id}"`)
