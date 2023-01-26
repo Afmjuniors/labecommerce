fef514f3360fcfae38b359676564e6e43ed5d8c4
Afmjuniors <afmjuniors@gmail.com>
2023-01-26 13:50:21 -0300

    feat: criação e refatoração dos endpoints

--- src/database/database.sql
+++ src/database/database.sql
@@ -5,41 +5,39 @@ CREATE TABLE users (
     name TEXT NOT NULL,
     email TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL,
-    modified_at TEXT DEFAULT(DATETIME()) NOT NULL,
-    create_at TEXT DEFAULT(DATETIME()) NOT NULL
+    modified_at TEXT NOT NULL,
+    create_at TEXT NOT NULL
 );
 CREATE TABLE products (
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
     name TEXT NOT NULL,
     price REAL NOT NULL,
-    category TEXT NOT NULL,
     description TEXT NOT NULL,
     url_image TEXT NOT NULL,
-    modified_at TEXT DEFAULT(DATETIME()) NOT NULL,
-    create_at TEXT DEFAULT(DATETIME()) NOT NULL
+    create_at TEXT NOT NULL,
+    modified_at TEXT NOT NULL,
+    category TEXT NOT NULL
    
 );
 CREATE TABLE purchases (
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
-    total_price REAL DEFAULT(0) NOT NULL ,
+    total_price REAL NOT NULL ,
     paid INTEGER DEFAULT(0) NOT NULL,
     delivered_at TEXT,
     buyer_id TEXT NOT NULL,
-    modified_at TEXT DEFAULT(DATETIME()) NOT NULL,
-    create_at TEXT DEFAULT(DATETIME()) NOT NULL,
+    create_at TEXT NOT NULL,
     FOREIGN KEY (buyer_id) REFERENCES users(id)
 );
-CREATE TABLE purchases_products(
+CREATE TABLE purchase_products(
     purchase_id TEXT NOT NULL,
     product_id TEXT NOT NULL,
     quantity INTEGER NOT NULL,
-    subtotal REAL NOT NULL,
     FOREIGN KEY (purchase_id) REFERENCES purchases(id),
     FOREIGN KEY (product_id) REFERENCES products(id)
 );
 
-DROP TABLE purchases_products;
+DROP TABLE purchase_products;
 DROP TABLE purchases;
 DROP TABLE products;
 DROP TABLE users;
-DROP TABLE testes;
+
